58ec5c1e3abba58ec1b0d81924bdf26c92ac9de0
Steph <39493915+princessmcmuffin@users.noreply.github.com>
2018-05-22 12:12:26 -0400

    Add files via upload

---
+++ c-to-f.js
@@ -0,0 +1,3 @@
+const celsuis = prompt('What is the celsuis temperature today?');
+const fahrenheit = celsuis * 9 / 5 + 32;
+console.log(`The temperature ${celsuis} degrees celsuis is ${fahrenheit} degrees fahrenheit.`);
---
+++ f-to-c.js
@@ -0,0 +1,3 @@
+const fahrenheit = prompt('What is the fahrenheit temperature today?');
+const celsuis = (fahrenheit - 32) * 5 / 9;
+console.log(`The temperature ${fahrenheit} degrees fahrenheit is ${celsuis} degrees celsuis.`);
